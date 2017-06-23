8b045b305eac8ee3e209ffd2dcd878e5873f78b4
nobukatsu <nobukatsu@nobukatsu.jp>
2017-06-23 17:47:41 +0900

    Get today's list

--- teacher_scraper.py
+++ teacher_scraper.py
@@ -1,3 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "http://eikaiwa.dmm.com/teacher/index/"
 
 def scrape():
-    print("scrape method")
+    ids = get_target_ids()
+    
+    for id in ids:
+        teacher_url = BASE_URL + id + "/"
+        
+        res = requests.get(teacher_url)
+        soup = BeautifulSoup(res.content, "html.parser")
+        
+        sche_list = soup.find("div", class_="schedules-list")
+        today = sche_list.find("ul", class_="oneday")
+        
+        print(today)
+
+
+def get_target_ids():
+    return ["4137"]
+
+
+if __name__ == "__main__":
+    scrape()
