914ccde4637ebbcd15c40560df785175441d0437
nobukatsu <nobukatsu@nobukatsu.jp>
2017-06-28 18:24:17 +0900

    Add a funciton of getting date

--- teacher_scraper.py
+++ teacher_scraper.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
 
 BASE_URL = "http://eikaiwa.dmm.com/teacher/index/"
 
 
 def scrape():
     ids = get_target_ids()
-    
+
+    now = datetime.now()
+    this_year = now.strftime("%Y")
+
     for id_ in ids:
         teacher_url = BASE_URL + id_ + "/"
         
@@ -16,11 +21,13 @@ def scrape():
         teacher_name = get_teacher_name(soup)
 
         schedule_list = soup.find("div", class_="schedules-list")
-        today = schedule_list.find("ul", class_="oneday")
+        today_column = schedule_list.find("ul", class_="oneday")
+        target_date = get_schedule_date(today_column.find("li").get_text(), this_year)
 
         print("id: " + id_)
         print("name: " + teacher_name)
-        print(today)
+        print(target_date)
+        print(today_column)
 
 
 def get_target_ids():
@@ -30,6 +37,13 @@ def get_target_ids():
 def get_teacher_name(soup):
     teacher_name = soup.find("h1")
     return teacher_name.get_text()
+
+
+def get_schedule_date(date_str, this_year):
+
+    schedule_date = datetime.strptime(this_year + "-" + date_str[0:2] + "-" + date_str[3:5], "%Y-%m-%d")
+
+    return schedule_date
 
 
 if __name__ == "__main__":
